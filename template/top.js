13f2aba6e675f6dcdcca2176de1899a57223ee1e
liangjunljlj <453021745@qq.com>
2018-04-11 15:59:47 +0800

    导航栏选中

--- template/top.js
+++ template/top.js
@@ -21,8 +21,8 @@ document.writeln("    	<span class=\'closeMenu\'>");
 document.writeln("    		<img src=\'../assets/images/sub_close.png\'/>");
 document.writeln("    	</span>");
 document.writeln("    	<ul>");
-document.writeln("    		<li><a href=\'../zh/index.html\'>主页</a></li>");
-document.writeln("    		<li><a href=\'../zh/home.html\'>下载</a></li>");
+document.writeln("    		<li class=\'index\'><a href=\'../zh/index.html\'>主页</a></li>");
+document.writeln("    		<li class=\'home\'><a href=\'../zh/home.html\'>下载</a></li>");
 document.writeln("    		<li><a href=\'../en/" + pathname + "\'>English</a></li>");
 document.writeln("    		<li><a href=\'../zh/" + pathname + "\'>中文</a></li>");
 document.writeln("    	</ul>");
@@ -30,9 +30,9 @@ document.writeln("    </div>");
 document.writeln("  </div>");
 document.writeln("  ");
 document.writeln("  <div class=\'eright pcView\'>");
-document.writeln("    <div onClick=" + "location.href=" + "'../zh/index.html' class=\'active\'>主页</div>");
-document.writeln("    <div onClick=" + "location.href=" + "'../zh/home.html'>下载</div>");
-document.writeln("    <div class=\'contact\'>联系我们</div>");
+document.writeln("    <div onClick=" + "location.href=" + "'../zh/index.html' class=\'index\'>主页</div>");
+document.writeln("    <div onClick=" + "location.href=" + "'../zh/home.html' class=\'home\'>下载</div>");
+/*document.writeln("    <div class=\'contact\'>联系我们</div>");*/
 document.writeln("    <div class=\'langChange\'>语言");
 document.writeln("      <span class=\'langBox\'>");
 document.writeln("        <p onClick=" + "location.href=" + "'../en/" + pathname + "'>ENGLISH</p>");
@@ -41,3 +41,12 @@ document.writeln("      </span>");
 document.writeln("    </div>");
 document.writeln("  </div>");
 document.writeln("</div>");
+
+$(function(){
+	var menuMap = {'home':'index','download':'home'};
+	Object.keys(menuMap).forEach(function(item){
+		if(new RegExp(item).test(location.pathname)){
+			$(".navBox ."+menuMap[item]).addClass("active");
+		}
+	})
+})
